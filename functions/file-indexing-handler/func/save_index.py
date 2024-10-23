a9f450bca754bf7860ddff5cdc7b9d226a8a5e96
jaemin <anjm1020@gmail.com>
2024-10-23 16:27:20 +0900

    feat: add indexing handler

---
+++ functions/file-indexing-handler/func/save_index.py
@@ -0,0 +1,59 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+from entity.index_data import IndexData
+
+from hooks.ssm_api import get_parameters, ParamRequest
+
+
+def save_index(data: IndexData):
+
+    params = get_opensearch_params()
+    USERNAME = params["opensearch_username"]
+    PASSWORD = params["opensearch_password"]
+    INDEX_NAME = params["opensearch_index_name"]
+    OPENSEARCH_HOST = params["opensearch_host"]
+
+    print(f"Indexed data: {data.to_index_format()}")
+    auth = HTTPBasicAuth(USERNAME, PASSWORD)
+
+    try:
+        url = f"{OPENSEARCH_HOST}/{INDEX_NAME}/_doc"
+        response = requests.post(
+            url=url,
+            auth=auth,
+            json=data.to_index_format(),
+            headers={"Content-Type": "application/json"},
+        )
+        response.raise_for_status()
+        print(f"Response status code: {response.status_code}")
+        print(f"Response JSON: {response.json()}")
+    except requests.exceptions.RequestException as e:
+        print(f"error with data: {data.to_index_format()}")
+        raise e
+
+
+def get_opensearch_params():
+    required_params: list[ParamRequest] = [
+        ParamRequest(
+            name="opensearch_username",
+            key="/findy/config/opensearch_username",
+            type="string",
+        ),
+        ParamRequest(
+            name="opensearch_password",
+            key="/findy/config/opensearch_password",
+            type="string",
+        ),
+        ParamRequest(
+            name="opensearch_index_name",
+            key="/findy/config/opensearch_index_name",
+            type="string",
+        ),
+        ParamRequest(
+            name="opensearch_host",
+            key="/findy/config/opensearch_host",
+            type="string",
+        ),
+    ]
+    return get_parameters(required_params=required_params)
---
+++ functions/file-indexing-handler/lambda_function.py
@@ -0,0 +1,59 @@
+import json
+import traceback
+import uuid
+
+from entity.formatted_data import FormattedData
+from entity.index_data import IndexData
+from func.invoke_embedding import invoke_embedding
+from func.save_index import save_index
+from func.mark_complete import mark_complete
+
+from entity.accessible_data import AccessibleData
+from hooks.sqs_api import (
+    ack_message,
+    get_queue_url_from_arn,
+    nack_message,
+    send_message,
+)
+from hooks.with_timeout import TimeoutException, with_timeout
+
+
+def process(record):
+    raw_data = record["body"]
+    if not isinstance(raw_data, dict):
+        raw_data = json.loads(raw_data)
+
+    data: FormattedData = FormattedData.from_dict(data=raw_data)
+    embeded_data: IndexData = invoke_embedding(data)
+    print("Completed invoke embedding")
+    save_index(embeded_data)
+    print("Completed save index")
+    object_key = str(uuid.uuid4())
+    mark_complete(
+        user_id=data.credentials.user_id,
+        service=data.credentials.service_type,
+        service_account=data.credentials.service_account,
+        version=data.version,
+        obj_key=object_key,
+        success=True,
+    )
+    return
+
+
+def handler(event, context):
+    print(f"Event: {event}")
+    for record in event["Records"]:
+        receipt_handle = record["receiptHandle"]
+        source_queue_url = get_queue_url_from_arn(record["eventSourceARN"])
+
+        try:
+            with_timeout(lambda: process(record), timeout=60)
+            ack_message(queue_url=source_queue_url, receipt_handle=receipt_handle)
+            print(f"Message processed successfully: {record['messageId']}")
+        except TimeoutException:
+            print(f"Timeout occurred while processing message: {record['messageId']}")
+            continue
+        except Exception as e:
+            print(f"Error processing message: {record['messageId']}, Error: {str(e)}")
+            traceback.print_exc()
+            nack_message(queue_url=source_queue_url, receipt_handle=receipt_handle)
