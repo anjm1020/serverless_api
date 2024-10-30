fa7bb5a824b93507792d410e8875c051b387335c
jaemin <anjm1020@gmail.com>
2024-10-30 14:41:04 +0900

    feat: add api gateway authorization handler

---
+++ api/auth/login/callback/lambda_function.py
@@ -0,0 +1,98 @@
+import traceback
+
+from dotenv import load_dotenv
+from func.login import login
+from google_auth_oauthlib.flow import Flow
+
+from hooks.account_db import Account
+from hooks.login_token import issue
+from hooks.ssm_api import ParamRequest, get_parameters
+
+load_dotenv(override=True)
+
+
+class CustomException(Exception):
+    @classmethod
+    def message(cls):
+        return "internal_server_error"
+
+
+class CodeMissingException(CustomException):
+    @classmethod
+    def message(cls):
+        return "Code missing in query parameters"
+
+
+class TokenConflictException(CustomException):
+    @classmethod
+    def message(cls):
+        return "Token already exists"
+
+
+def handler(event, context):
+    try:
+        service_type = "google-login"
+        required_params: list[ParamRequest] = [
+            ParamRequest(
+                name="client_secret",
+                key=f"/oauth/{service_type}/client_secret",
+                type="json",
+            ),
+            ParamRequest(
+                name="config", key=f"/oauth/{service_type}/config", type="json"
+            ),
+            ParamRequest(
+                name="login_success_url",
+                key="/oauth/common/login_success_url",
+                type="string",
+            ),
+            ParamRequest(
+                name="queue_url",
+                key="/oauth/common/credential_queue_url",
+                type="string",
+            ),
+        ]
+
+        params = get_parameters(required_params=required_params)
+        client_secret = params["client_secret"]
+        oauth_config = params["config"]
+        login_success_url = params["login_success_url"]
+
+        scopes = oauth_config["scopes"]
+        redirect_uri = oauth_config["redirect_uri"]
+
+        code = event["queryStringParameters"]["code"]
+        state = event["queryStringParameters"]["state"]
+
+        if not code:
+            raise CodeMissingException()
+
+        flow = Flow.from_client_config(
+            client_config=client_secret,
+            scopes=scopes,
+            redirect_uri=redirect_uri,
+            state=state,
+        )
+
+        print("Start Fetching token")
+        flow.fetch_token(code=code)
+        credentials = flow.credentials
+
+        account: Account = login(credentials)
+        token = issue(account.uid)
+        return {
+            "statusCode": 302,
+            "headers": {"Location": f"{login_success_url}?token={token}"},
+        }
+    except Exception as e:
+        print(f"Error: {e}")
+        print(f"Traceback: {traceback.format_exc()}")
+
+        error_msg = "internal_server_error"
+        if isinstance(e, CustomException):
+            error_msg = e.message()
+
+        return {
+            "statusCode": 302,
+            "headers": {"Location": f"{login_success_url}?error={error_msg}"},
+        }
---
+++ hooks/login_token.py
@@ -0,0 +1,32 @@
+import datetime
+
+import jwt
+
+from hooks.ssm_api import ParamRequest, get_parameters
+
+params = get_parameters(
+    [
+        ParamRequest(key="/oauth/jwt_key", name="jwt_key", type="str"),
+        ParamRequest(key="/oauth/jwt_exp", name="jwt_exp", type="str"),
+    ]
+)
+key = params["jwt_key"]
+delta = int(params["jwt_exp"])
+
+
+def issue(user_uid):
+    exp = datetime.datetime.utcnow() + datetime.timedelta(seconds=delta)
+    return jwt.encode(
+        payload={"exp": exp, "user_uid": user_uid}, key=key, algorithm="HS256"
+    )
+
+
+def validate(token):
+    try:
+        print(f"validated token={token}")
+        result = jwt.decode(token, key=key, algorithms="HS256")
+    except Exception as e:
+        print(f"Decode Exception : {e}")
+        return None
+    else:
+        return result
